abc96cda2e811d64d9f026c74bc94f067d80d4b9
Ruben <raalfordm@unadvirtual.edu.co>
2024-01-31 12:06:12 -0500

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { PersonajesComponent } from './components/personajes/personajes.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'personajes', component: PersonajesComponent },
+  { path: 'detalles/:id', component: DetalleComponent },
+];
